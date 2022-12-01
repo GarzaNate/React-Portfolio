85075524a09f4565bf31cd7e32760f3718e1d370
slendyG <josuegarza0608@gmail.com>
2022-11-30 21:52:27 -0800

    additions to About component

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,13 @@ function About() {
 
     return (
         <div>
-            
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">About Me</h1>
+                    <p className="lead">Recent BootCamp graduate seeking an entry level full-stack developer position with a technically innovative company. I am highly collaborative and a creative problem solver. My strong work ethic along with my innate ability to bring energy to the workplace makes me an excellent addition to any team!
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
